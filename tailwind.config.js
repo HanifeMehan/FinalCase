4119d62f310ad158f72ee5d218ea7a5eeeda3990
HanifeMehan <hanifemehan0853@gmail.com>
2023-04-28 22:26:51 +0300

    tailwind css configurations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 module.exports = {
-    plugins: [
-      require('tailwindcss'),
-      require('autoprefixer'),
-    ]
-  }
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
