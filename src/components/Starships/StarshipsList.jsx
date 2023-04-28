46b778eff7bfa84e77b7c7dcf7b1ca36a1e40464
HanifeMehan <hanifemehan0853@gmail.com>
2023-04-28 22:02:20 +0300

    starship list review

--- src/components/Starships/StarshipsList.jsx
+++ src/components/Starships/StarshipsList.jsx
@@ -5,7 +5,15 @@ import Loading from "../Loader/Loader";
 import "./StarshipsList.css";
 
 const StarshipList = () => {
-  const { starShips, loading, resultTitle } = useGlobalContext();
+  const {
+    starShips,
+    loading,
+    resultTitle,
+    isFetching,
+    isFetchingNextPage,
+    hasNextPage,
+    fetchNextPage,
+  } = useGlobalContext();
 
   //Veriler alınana kadarki süreç içim loading eklendi
   if (loading) return <Loading />;
@@ -17,9 +25,25 @@ const StarshipList = () => {
           <h2>{resultTitle}</h2>
         </div>
         <div className="starshiplist-content grid">
-          {starShips.slice(0, 30).map((item, index) => {
+          {starShips.slice(0, 40).map((item, index) => {
             return <Starships key={index} item={item} />;
           })}
+        </div>
+        <div className="flex justify-center items-center">
+          {isFetching && !isFetchingNextPage && (
+            <p>Fetching more starships...</p>
+          )}
+          <button
+            className="loadMoreBtn"
+            onClick={() => fetchNextPage()}
+            disabled={!hasNextPage || isFetchingNextPage}
+          >
+            {isFetchingNextPage
+              ? "Loading more starships..."
+              : hasNextPage
+              ? "Load more starships..."
+              : "All starships loaded"}
+          </button>
         </div>
       </div>
     </section>
