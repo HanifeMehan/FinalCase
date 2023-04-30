4c8617ac8bb2c676a660b9393a012758c1b71971
HanifeMehan <hanifemehan0853@gmail.com>
2023-04-30 18:35:59 +0300

    footer text review

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,7 +5,9 @@ const Footer = () => {
   return (
     <div className="footer-container">
       <div className="footer-bottom">
-        <span className="footerName">Coded by Hanife Mehan</span>
+        <span className="footerName">
+          Coded by <a href="https://github.com/HanifeMehan">Hanife Mehan</a>
+        </span>
       </div>
     </div>
   );
